29f2f9590fc56182d95ece41825c2fcbe851ae50
Jonathan Ward <jon.patrick.ward@gmail.com>
2018-06-11 18:48:48 -0400

    google news api works now

--- newsAPIConnect.js
+++ newsAPIConnect.js
@@ -2,10 +2,12 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('67e11e145e34441bb45b4e4e2d7e920d');
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
+newsapi.v2.everything({
   q: 'fortnite',
-  language: 'en',
-  country: 'us'
+  sources: "abc-news, ign, google-news",
+  pageSize: 10,
+  from: "2018-06-08",
+  language: 'en'
 }).then(response => {
   console.log(response);
   /*
