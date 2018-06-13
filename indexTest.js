6aebac5d659b844b274e7f0fdf3a64c7a1385f29
Jonathan Ward <jon.patrick.ward@gmail.com>
2018-06-13 00:12:45 -0400

    messing around with trying to get scraper to work

--- indexTest.js
+++ indexTest.js
@@ -1,15 +1,84 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const options = {
-  uri: `https://www.jopatwa.com`,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
+const optionsArray = [
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-1`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-2`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-3`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-4`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-5`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-6`,
+        transform: function (body) { return cheerio.load(body); }
+    },
+    options = {
+        uri: `https://db.fortnitetracker.com/challenges/week-7`,
+        transform: function (body) { return cheerio.load(body); }
+    }
+]
+const challenges = [];
+optionsArray.forEach(function (options) {
+    rp(options)
+        .then(function ($) {
+
+            $('.panel-heading').each(function (i, elem) {
+                var challenge = $(this).text();
+                challenges[i] = challenge.replace(/(\r\n\t|\n|\r\t)/gm, "");
+            })
+            console.log(challenges);
+            return challenges;
+        })
+        .catch(function (err) {
+            return err;
+            console.log(err);
+        });
+
+    // rp(options)
+    // .then(function ($) {
+    //     const challenges = [];
+    //     $('.quest_text').each(function (i, elem) {
+    //         var challenge = $(this).text();
+    //         challenges[i] = challenge.replace(/(\r\n\t|\n|\r\t)/gm, "");
+    //     })
+    //     console.log(challenges);
+    //     return challenges;
+    // })
+    // .catch(function (err) {
+    //     return err;
+    //     console.log(err);
+    // });
+
+});
+
+
 rp(options)
-    .then(function (data) {
-        console.log(data);
+    .then(function ($) {
+        const challenges = [];
+        $('.quest_text').each(function (i, elem) {
+            var challenge = $(this).text();
+            challenges[i] = challenge.replace(/(\r\n\t|\n|\r\t)/gm, "");
+        })
+        console.log(challenges);
+        return challenges;
     })
     .catch(function (err) {
+        return err;
         console.log(err);
     });
+
+
+
